0051ef87524021157161301f07a5a4cdec5cad45
Thomas Massmann <thomas@propertyshelf.com>
2013-10-17 11:25:14 +0200

    Fixed link to readme.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='theming.toolkit.viewlets',
       version=version,
       description="A product that allows to activate a set of viewlets for Plone content items.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
