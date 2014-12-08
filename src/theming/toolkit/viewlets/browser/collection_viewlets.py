7fc81a270ec77075d12eb5ced72b11f0352c8717
Jens Krause <jens@propertyshelf.com>
2014-12-08 19:21:46 +0700

    introduce a Class to configure the the intended position of the slider. this can be used via Diazo rule to move the content outside of the viewlet manager.

--- src/theming/toolkit/viewlets/browser/collection_viewlets.py
+++ src/theming/toolkit/viewlets/browser/collection_viewlets.py
@@ -44,6 +44,8 @@ MLS_IMAGE_SIZES = ['thumb', 'mini', 'preview', 'large']
 SLIDER_STEPS = ["1", "2", "3", "4", "5","6","7", "8", "9", "10"]
 SLIDER_STEPS_FULL = ["0", "1", "2", "3", "4", "5","6","7", "8", "9", "10"]
 LARGE_RANGE = ["0", "1", "2", "3", "4", "5","6","7", "8", "9", "10", "11", "12", "13", "14", "15","16","17", "18", "19", "20", "21", "22", "23", "24", "25","26","27", "28", "29", "30", "31", "32", "33", "34", "35","36","37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50","51", "52", "53", "54", "55","56","57", "58", "59", "60", "61", "62", "63", "64", "65","66","67", "68", "69", "70", "71", "72", "73", "74", "75","76","77", "78", "79", "80", "81", "82", "83", "84", "85","86","87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
+SLIDER_POSITION_TOP = ['aboveMenue', 'aboveColumns', 'aboveContentArea', 'portletLeft', 'portletRight']
+SLIDER_POSITION_BOTTOM = ['belowContentArea', 'belowColumns', 'aboveColophone','belowFooter', 'portletLeft', 'portletRight']
 
 ARROW_STYLE ={}
 ARROW_STYLE['arrow01']='<span u="arrowleft" class="jssora01l" style="width: 45px; height: 45px; top: 127.5px; left: 8px;"/><span u="arrowright" class="jssora01r" style="width: 45px; height: 45px; top: 127.5px; right: 8px;"/>'
@@ -502,6 +504,16 @@ class IItemProvider(Interface):
         ),
     )
 
+    viewlet_positionClass = schema.Choice(
+        title=_(u'Slider Position'),
+        description=_(
+            u'Choose the Position Class for this slider. This is used to place it in different areas in your theme.'
+        ),
+        required=True,
+        default= 'aboveContentArea',
+        values= SLIDER_POSITION_TOP
+    )
+
     featuredListingSlider_ItemList = schema.TextLine(
         required=True,
         title=_(
@@ -525,7 +537,6 @@ class IItemProvider(Interface):
         ),
     )
 
-   
     featuredListingSlider_Limit =schema.TextLine(
         default=u"",
         required=False,
