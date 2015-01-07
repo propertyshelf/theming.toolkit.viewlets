7c9c93836bbb1ad2c7b331ee8cb0eefef70858a3
Jens Krause <jens@propertyshelf.com>
2015-01-07 13:58:51 +0700

    update Diazo Position Classes

--- src/theming/toolkit/viewlets/browser/collection_viewlets.py
+++ src/theming/toolkit/viewlets/browser/collection_viewlets.py
@@ -52,7 +52,7 @@ MLS_IMAGE_SIZES = ['thumb', 'mini', 'preview', 'large']
 SLIDER_STEPS = ["1", "2", "3", "4", "5","6","7", "8", "9", "10"]
 SLIDER_STEPS_FULL = ["0", "1", "2", "3", "4", "5","6","7", "8", "9", "10"]
 LARGE_RANGE = ["0", "1", "2", "3", "4", "5","6","7", "8", "9", "10", "11", "12", "13", "14", "15","16","17", "18", "19", "20", "21", "22", "23", "24", "25","26","27", "28", "29", "30", "31", "32", "33", "34", "35","36","37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50","51", "52", "53", "54", "55","56","57", "58", "59", "60", "61", "62", "63", "64", "65","66","67", "68", "69", "70", "71", "72", "73", "74", "75","76","77", "78", "79", "80", "81", "82", "83", "84", "85","86","87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
-SLIDER_POSITION_TOP = ['aboveMenue', 'aboveColumns', 'aboveContentArea', 'portletLeft', 'portletRight']
+SLIDER_POSITION_TOP = ['aboveMenue', 'aboveColumns', 'aboveContent', 'portletLeft', 'portletRight', 'coverIntegrated']
 SLIDER_POSITION_BOTTOM = ['belowContentArea', 'belowColumns', 'aboveColophone','belowFooter', 'portletLeft', 'portletRight']
 
 ARROW_STYLE ={}
@@ -214,7 +214,7 @@ class FeaturedListingCollectionViewlet(ViewletBase):
     def get_PositionClass(self):
         """Set CSS marker class for Diazo Rules"""
         settings = self.Settings
-        return settings.get('viewlet_positionClass','aboveContentArea')
+        return settings.get('viewlet_positionClass','aboveContent')
 
     @property
     def get_ArrowNavigator(self):
@@ -362,7 +362,7 @@ class FeaturedListingCollectionViewlet(ViewletBase):
             return provider.results()
         except Exception, e:
             """Don't have .results()"""
-            print e
+            pprint(e)
         try:
             return provider.queryCatalog()
         except Exception, e:
@@ -541,7 +541,7 @@ class IItemProvider(Interface):
             u'Choose the Position Class for this slider. This is used to place it in different areas in your theme.'
         ),
         required=True,
-        default= 'aboveContentArea',
+        default= 'aboveContent',
         values= SLIDER_POSITION_TOP
     )
 
